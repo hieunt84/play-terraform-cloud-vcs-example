e10f3e5a9b18926cf9a0a4fc1e83dae71c8333ff
Focus <hieunt9@gmail.com>
2022-05-02 12:32:21 +0700

    fix error

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+
 provider "aws" {
   profile = "default"
-  region = var.region
+  region = "us-west-2"
 }
 
 /*data "aws_ami" "ami" {
